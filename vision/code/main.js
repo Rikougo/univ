79e37a97bcf524693deb27202e27f88a2e8a74c0
Elye <samhelye@gmail.com>
2022-10-26 19:39:16 +0200

    Optical flow update (last)

--- vision/code/main.js
+++ vision/code/main.js
@@ -1,63 +1,146 @@
 let zoom = 1;
+let images = [];
+let currentCVImage;
 
-window.onload = () => {
-    document.getElementById("zoomRange").oninput = OnZoomChange;
-    document.getElementById("traceButton").onclick = OnDrawLine;
-}
+let topleftbb = {x: 0, y: 0};
+let bottomrightbb = {x: 0, y: 0};
+
+let validated = false;
+let bb;
+let selected_pattern;
+let A;
+let A_invert;
 
 const OnOpenCVReady = () => {
+    document.getElementById('status').innerHTML = 'OpenCV.js is ready.';
+
     let imgElement = document.getElementById('imageSrc');
     let inputElement = document.getElementById('fileInput');
+
+    let validateButtonElement = document.getElementById('validateButton');
+    validateButtonElement.addEventListener("click", InitTracking);
+    
+    /**
+     * @type{HTMLInputElement}
+     */
+    let imageSliderElement = document.getElementById('imgSlider');
+    imageSliderElement.disabled = true;
     inputElement.addEventListener('change', (e) => {
-        imgElement.src = URL.createObjectURL(e.target.files[0]);
+        inputElement.disabled = true;
+        images = e.target.files;
+        imgElement.src = URL.createObjectURL(images[0]);
+
+        imageSliderElement.min = 0; imageSliderElement.max = images.length - 1;
+        imageSliderElement.value = 0;
+        imageSliderElement.addEventListener('change', ChangeImage);
     }, false);
+
     imgElement.onload = function () {
-        let mat = cv.imread(imgElement);
-        cv.imshow('canvasOutput', mat);
-        document.getElementById("canvasOutput").onclick = OnMouseCallback;
-    };
-    var Module = {
-        // https://emscripten.org/docs/api_reference/module.html#Module.onRuntimeInitialized
-        onRuntimeInitialized() {
-            document.getElementById('status').innerHTML = 'OpenCV.js is ready.';
+        UpdateCVImage();
+        
+        if (validated) {   
+            bb = OpticalFlow(A_invert, currentCVImage, bb, selected_pattern);
+
+            DrawSelectedRectangleOnCVImage();
         }
+        
+        ShowCurrentImage();
+
+        if (!validated) document.getElementById("canvasOutput").onclick = OnMouseCallback;
     };
 }
 
-const OnZoomChange = (event) => {
-    zoom = parseFloat(event.target.value);
-    document.getElementById("zoomText").innerHTML = zoom;
-
+const UpdateCVImage = () => {
     let imgElement = document.getElementById('imageSrc');
     let source = cv.imread(imgElement);
-    let dest = new cv.Mat();
-    let dsize = new cv.Size(source.size().width * zoom, source.size().height * zoom);
-
-    cv.resize(source, dest, dsize, 0, 0);
-    cv.imshow('canvasOutput', dest);
+    currentCVImage = new cv.Mat();
+    cv.cvtColor(source, currentCVImage, cv.COLOR_RGBA2GRAY, 0); 
 }
 
-const OnDrawLine = () => {
-    const [a, b, c] = [
-        parseFloat(document.getElementById('aInput').value),
-        parseFloat(document.getElementById('bInput').value),
-        parseFloat(document.getElementById('cInput').value)
-    ];
+const DrawSelectedRectangleOnCVImage = () => {
+    console.log(bb);
+    cv.rectangle(currentCVImage, bb.topLeft, bb.bottomRight, new cv.Scalar(255, 0, 0, 255));
+}
 
+const ShowCurrentImage = () => {
+    cv.imshow('canvasOutput', currentCVImage);
+}
+
+const InitTracking = (p_event) => {
+    console.log("Validating selection...");
+    validated = true;
+    document.getElementById('validateButton').disabled = true;
+    document.getElementById("clicked").innerHTML = "";
+
+
+    /**
+     * @type{HTMLInputElement}
+     */
+     let imageSliderElement = document.getElementById('imgSlider');
+     imageSliderElement.disabled = false;
+
+    console.log("Creating bouding box...")
+    bb = new BoundingBox(topleftbb, bottomrightbb);
+
+    console.log("Extract pattern...")
+    selected_pattern = ExtracPattern(currentCVImage, bb);
+
+    cv.imshow('patternPreview', selected_pattern);
+
+    console.log("Compute A inverse...");
+    let dstx = new cv.Mat();
+    let dsty = new cv.Mat();
+
+    cv.Sobel(selected_pattern, dstx, cv.CV_8U, 1, 0, 3, 1, 0, cv.BORDER_DEFAULT);
+    cv.Sobel(selected_pattern, dsty, cv.CV_8U, 0, 1, 3, 1, 0, cv.BORDER_DEFAULT);
+
+    cv.imshow('gradientXPreview', dstx);
+    cv.imshow('gradientYPreview', dsty);
+
+    dstx = Flatten(dstx);
+    dsty = Flatten(dsty);
+    let gradient = new cv.Mat(dstx.rows, 2, dstx.type());
+
+    for(let i = 0; i < dstx.rows; i++) {
+        gradient.data[i*2] = dstx.data[i];
+        gradient.data[i*2+1] = dsty.data[i];
+    }
+
+    console.log(dstx.size(), dsty.size(), gradient.size());
+
+    A = gradient;
+    A_invert = new cv.Mat();
+    let _32fmat = new cv.Mat();
+    A.convertTo(_32fmat, cv.CV_32F);
+    cv.invert(_32fmat, A_invert, cv.DECOMP_SVD);
+    console.log(A_invert.size());
+
+    console.log("Tracking initialized.");
+}
+
+const ChangeImage = (p_event) => {
     let imgElement = document.getElementById('imageSrc');
-    let source = cv.imread(imgElement);
-    cv.line(source, p1, p2, [0, 255, 0, 255], 1);
-    cv.imshow('canvasOutput', source);
+    imgElement.src = URL.createObjectURL(images[parseInt(p_event.target.value)]);
 }
 
 const OnMouseCallback = (event) => {
-    const rect = event.target.getBoundingClientRect();
-    const [x, y] = [
-        Math.floor((event.clientX - rect.left) / zoom),
-        Math.floor((event.clientY - rect.top) / zoom)
-    ];
-    document.getElementById("clicked").innerHTML = `Clicked ${x}, ${y}`;
-    let imgElement = document.getElementById('imageSrc');
-    let source = cv.imread(imgElement);
-    console.log(source.ucharPtr(y, x));
+    if (!validated) {
+        const rect = event.target.getBoundingClientRect();
+        const [x, y] = [
+            Math.floor((event.clientX - rect.left)),
+            Math.floor((event.clientY - rect.top))
+        ];
+    
+        bottomrightbb = topleftbb;
+        topleftbb = {x: x, y: y};
+
+        bb = new BoundingBox(topleftbb, bottomrightbb);
+        console.log(bb);
+    
+        document.getElementById("clicked").innerHTML = `Clicked ${x}, ${y}`;
+    
+        UpdateCVImage();
+        DrawSelectedRectangleOnCVImage();
+        ShowCurrentImage();
+    }
 }
--- vision/code/optical_flow.js
+++ vision/code/optical_flow.js
@@ -1,9 +1,90 @@
+class BoundingBox {
+    /**
+     * 
+     * @param {{x: number, y: number}} p_topLeft
+     * @param {{x: number, y: number}} p_bottomRight
+     */
+    constructor(p_topLeft, p_bottomRight) {
+        this.topLeft = p_topLeft;
+        this.bottomRight = p_bottomRight;
+    }
+
+    width() {
+        return this.topLeft.x - this.bottomRight.x;
+    }
+
+    height() {
+        return this.topLeft.y - this.bottomRight.y;
+    }
+
+    translate(u, v) {
+        return new BoundingBox(
+            {x: this.topLeft.x + u, y: this.topLeft.y + v},
+            {x: this.bottomRight.x + u, y: this.bottomRight.y + v},
+        );
+    }
+}
+
+const ExtracPattern = (p_source, p_boundingBox) => {
+    let pattern = new cv.Mat(p_boundingBox.height(), p_boundingBox.width(), p_source.type());
+
+    for(let i = p_boundingBox.bottomRight.x; i < p_boundingBox.topLeft.x; i++) {
+        for(let j = p_boundingBox.bottomRight.y; j < p_boundingBox.topLeft.y; j++) {
+            let sI = i - p_boundingBox.bottomRight.x, sJ = j - p_boundingBox.bottomRight.y;
+            pattern.data[sI + sJ * pattern.cols] = p_source.data[i + j * p_source.cols];
+        }
+    }
+
+    return pattern;
+};
+
+const Flatten = (p_source) => {
+    let l_flatten = new cv.Mat(p_source.rows * p_source.cols, 1, p_source.type());
+
+    for(let i = 0; i < p_source.rows * p_source.cols; i++) l_flatten.data[i] = p_source.data[i];
+
+    return l_flatten;
+}
+
+const MatMul = (p_A, p_B) => {
+    if (p_A.size().width != p_B.size().height) {
+        console.error("Incompatible size", p_A.size(), p_B.size());
+        return;
+    }
+
+    let l_result = new cv.Mat(p_A.rows, p_B.cols, p_A.type());
+    for(let i = 0; i < l_result.rows * l_result.cols; i++) l_result.data[i] = 0;
+
+    for (let i = 0; i < p_A.rows; i++) {
+        for (let k = 0; k < p_B.cols; k++) {
+            for (let j = 0; j < p_A.cols; j++) {
+                l_result.data[i * l_result.cols + k] += p_A.data[i * p_A.cols + j] * p_B.data[j * p_B.cols + k];
+            }
+        }
+    }
+
+    return l_result;
+}
+
 /**
  * 
  * @param {} p_previousImg 
  * @param {} p_nextImg 
- * @param {} p_targetPoint 
+ * @param {BoundingBox} p_boundingBox 
  */
-const OpticalFlow = (p_previousImg, p_nextImg, p_targetBox, p_imageGX, p_imageGY) => {
+const OpticalFlow = (p_ApseudoInvert, p_nextImg, p_boundingBox, p_baseBattern) => {
+    let l_extractedPattern = ExtracPattern(p_nextImg, p_boundingBox);
 
+    let l_B = new cv.Mat();
+    let l_mask = new cv.Mat();
+    let l_dtype = cv.CV_32F;
+    cv.subtract(l_extractedPattern, p_baseBattern, l_B, l_mask, l_dtype);
+    
+    let l_flatB = Flatten(l_B);
+
+    console.log(l_flatB.size(), p_ApseudoInvert.size(), l_flatB.type(), p_ApseudoInvert.type(), cv.CV_32F);
+
+    let l_UV = MatMul(p_ApseudoInvert, l_flatB);
+    console.log(l_UV.data[0], l_UV.data[1]);
+    return p_boundingBox.translate(l_UV.data[0], l_UV.data[1]);
 }
